746f0dbeeff48ad7bd3fc00147006e37aebffde8
lovesh <lovesh.bond@gmail.com>
2019-07-01 02:02:47 +0530

    types not working
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

---
+++ src/errors.rs
@@ -0,0 +1,41 @@
+/*error_chain! {
+    errors {
+        MoreThanSupported(T: u128) {
+            description("Time more than the maximum supported value")
+            display("T should be less than max value for u128 but was : {}", T)
+        }
+    }
+}*/
+
+use failure::Error;
+
+#[derive(Debug, Fail)]
+pub enum PixelError {
+    #[fail(display = "T should be less than max value for u128 but was : {}", T)]
+    MoreThanSupported {
+        T: u128,
+    },
+    //#[fail(display = "T+1 should be power of 2 but T was : {}", T+1)]
+    #[fail(display = "T+1 should be power of 2")]
+    NonPowerOfTwo {
+        T: u128,
+    },
+    #[fail(display = "Invalid path={:?} for l={}", path, l)]
+    InvalidPath {
+        path: Vec<u8>,
+        l: u8,
+    },
+    #[fail(display = "Invalid node number={} for l={}", t, l)]
+    InvalidNodeNum {
+        t: u128,
+        l: u8,
+    },
+    #[fail(display = "Provide at least {} generators", n)]
+    GeneratorsLessThanMinimum {
+        n: u8
+    },
+    #[fail(display = "Sigkey for time t={} not found", t)]
+    SigkeyNotFound {
+        t: u128
+    },
+}
